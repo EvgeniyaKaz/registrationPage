51cb01926304602f13654fee6484672af78f2eb0
Zhenya <kerrigan.vig56@gmail.com>
2025-01-21 22:23:01 +0300

    add finall project React Hook Form and Yup

--- registration-page/src/App.js
+++ registration-page/src/App.js
@@ -1,6 +1,7 @@
-import * as yup from "yup";
+import { useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { scheme } from "./form-scheme";
 import styles from "./App.module.css";
 
 const initialState = {
@@ -9,69 +10,31 @@ const initialState = {
 	repeatPassword: "",
 };
 
-const scheme = yup.object().shape({
-	email: yup
-		.string()
-		.matches(
-			/^[\w_.@]*$/,
-			"Неверный email. Допустимые символы: английские буквы, цифры, точка и нижнее подчеркивание."
-		)
-		.max(
-			20,
-			"Неверный email. Длина email должна быть не больше 20 символов."
-		)
-		.min(
-			2,
-			"Неверный email. Длина email должна быть не меньше 2 символов."
-		),
-	password: yup
-		.string()
-		.matches(
-			/^[\w]*$/,
-			"Неверный пароль. Допустимые символы: цифры и английские буквы."
-		)
-		.max(
-			20,
-			"Неверный пароль. Длина пароля должна быть не больше 20 символов."
-		)
-		.min(
-			5,
-			"Неверный пароль. Длина пароля должна быть не менее 5 символов."
-		),
-	repeatPassword: yup
-		.string()
-		.matches(
-			/^[\w]*$/,
-			"Неверный пароль. Допустимые символы: цифры и английские буквы."
-		)
-		.oneOf([yup.ref("password")], "Пароли не совпадают")
-		.max(
-			20,
-			"Неверный пароль. Длина пароля должна быть не больше 20 символов."
-		)
-		.min(
-			5,
-			"Неверный пароль. Длина пароля должна быть не менее 5 символов."
-		),
-});
-
 function App() {
 	const {
 		register,
 		handleSubmit,
-		formState: { errors },
+		formState: { isValid, errors },
 	} = useForm({
 		defaultValues: initialState,
 		resolver: yupResolver(scheme),
+		mode: "onTouched",
 	});
 
 	const emailError = errors.email?.message;
 	const passwordError = errors.password?.message;
 	const repeatPassword = errors.repeatPassword?.message;
 
-	const sendData = (formData) => {
-		console.log(formData);
+	const sendData = ({ email, password }) => {
+		console.log({ email, password });
 	};
+
+	const submitButtonRef = useRef(null);
+	useEffect(() => {
+		if (isValid) {
+			submitButtonRef.current.focus();
+		}
+	}, [isValid]);
 
 	return (
 		<div className={styles.container}>
@@ -84,7 +47,6 @@ function App() {
 				</header>
 				<input
 					type="email"
-					name="email"
 					placeholder="Введите email"
 					className={styles["container-form-input"]}
 					{...register("email")}
@@ -92,7 +54,6 @@ function App() {
 				<br />
 				<input
 					type="password"
-					name="password"
 					placeholder="Введите пароль"
 					className={styles["container-form-input"]}
 					{...register("password")}
@@ -100,7 +61,6 @@ function App() {
 				<br />
 				<input
 					type="password"
-					name="repeatPassword"
 					placeholder="Повторите пароль"
 					className={styles["container-form-input"]}
 					{...register("repeatPassword")}
@@ -109,13 +69,12 @@ function App() {
 				<button
 					type="submit"
 					className={
-						!!emailError || !!passwordError || !!repeatPassword
+						!isValid
 							? styles["container-form-button-block"]
 							: styles["container-form-button"]
 					}
-					disabled={
-						!!emailError || !!passwordError || !!repeatPassword
-					}
+					disabled={!isValid}
+					ref={submitButtonRef}
 				>
 					Зарегистрироваться
 				</button>
